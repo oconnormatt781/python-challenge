7d243e99651988602c8b9dcb998de4f5564894ce
oconnormatt781 <oconnor.matt781@gmail.com>
2020-03-27 18:58:55 -0500

    Week 3 homework - Python

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,24 +2,34 @@
 import os
 import csv
 
+# Count months
+
 file = open("budget_data.csv")
 reader = csv.reader(file)
 months = len(list(reader))
+
+# Sum total profit/loss
 
 file = open("budget_data.csv")
 reader = csv.DictReader(file)
 profit = 0
 profit = sum(float(row["Profit/Losses"]) for row in reader)
 
+# Find greatest increase
+
 file = open("budget_data.csv")
 reader = csv.DictReader(file)
 greatest_increase = 0
 greatest_increase = max(float(row["Profit/Losses"]) for row in reader)
 
+# Find greatest decrease
+
 file = open("budget_data.csv")
 reader = csv.DictReader(file)
 greatest_decrease = 0
 greatest_decrease = min(float(row["Profit/Losses"]) for row in reader)
+
+# Print results (month - 1 to exclude headers)
 
 print("Financial Analysis")
 print ("Months: ", (months - 1))
@@ -27,3 +37,11 @@ print ("Total Profit/Loss: ", (profit))
 print ("Average Change: ", profit / (months - 1))
 print ("Greatest Increase ", greatest_increase)
 print ("Greatest Decrease ", greatest_decrease)
+
+with open("output.txt", "w") as text_file:
+    print(f"Months: ", (months - 1),
+    "Total Profit/Loss", (profit),
+    "Average Change: ", (profit / (months - 1)),
+    "Greatest Increase: ", (greatest_increase),
+    "Greatest Decrease: ", (greatest_decrease),
+    file=text_file)
---
+++ PyPoll/main.py
@@ -0,0 +1,27 @@
+# Imports
+import os
+import csv
+import collections
+
+Khan = 0
+Correy = 0
+Li = 0
+OTooley = 0
+
+file = open("election_data.csv")
+reader = csv.reader(file)
+votes = len(list(reader))
+
+my_reader = csv.reader(open("election_data.csv"))
+khanvotes = 0
+for record in my_reader:
+    if record[1] == "Khan":
+        khanvotes += 1
+
+from collections import Counter
+Candidate = [rec[3] for rec in my_reader]
+result = Candidate.count('Khan')
+print(result)
+
+print("Total Votes: ", votes - 1)
+print("Khan Votes: ", khanvotes)
